483d42b015808e440d6a7458648629b34bce01aa
Katie Bolton <54602260+kgbolton@users.noreply.github.com>
2021-04-08 16:52:16 -0700

    update dump

--- census.py
+++ census.py
@@ -1,3 +1,5 @@
+# testing census class functions
+
 from itertools import product
 from functools import reduce
 from multiprocessing import Pool
@@ -59,8 +61,8 @@ class CensusViewer:
         Does some optimization to run census api queries in parallel. Consider tweaking 
         N_PROCESSES parameter to affect performance.
         args:
-            county_names (List[str]): List of state, county name pairs
-            selected_vars (List[Dict]): List of variable dicts
+            county_names (List[str]): List of county names and their state
+            selected_cats (List[str]): List of variable categories of interest
             descriptions (boolean): Boolean controlling whether to include variable
                 descriptions in df output (not implemented)
             src (str): Census api source parameter       
@@ -82,7 +84,7 @@ class CensusViewer:
         # censusdata.download for each year x tabletype.  
 
         # So:
-        # 1. get state & county fip - county formatted as list for potential update to allow users to select multiple counties
+        # 1. get state fip
 
         state_fip = list(state_county_fips[state_county_fips['State'].isin(states)].State_FIPS.unique())[0]
         if len(state_fip)==1:
@@ -122,7 +124,7 @@ class CensusViewer:
 
         raw_dfs = pool.starmap(self._call_census, census_jobs)
 
-        # 5. filter counties and merge all
+        # 5. filter counties and merge all - county formatted as list for potential update to allow users to select multiple counties
         geos = []
         for geo in raw_dfs[0][0].index.tolist():
             if geo.name in county_names:
@@ -147,3 +149,137 @@ class CensusViewer:
         raw_data['Category'] = raw_data.apply(lambda x: vardf[vardf['vars'] == x.name]['category'].iloc[0], axis=1)
 
         return raw_data
+
+    @staticmethod
+    def _call_fortracts(state_fip, county_fip, var_ids, src, year, tabletype, api_key):
+        """
+        Queries census API for tract-level data
+        """
+
+        # build list of var ids, and dict of id-name mappings
+
+        tract_data = censusdata.download(
+            src,
+            year,
+            censusdata.censusgeo([("state", state_fip), ("county", county_fip), ("tract", "*")]),
+            var_ids,
+            key=api_key,
+            tabletype=tabletype,
+        )
+        
+
+        return tract_data, year
+
+    def build_mapping_df(
+        self, county_names, state, selected_cats, descriptions=False, src="acs5"):
+        """
+        Creates dataframe view of variables in requested counties. Main helper 
+        view function, ie does most of the work of munging frontend queries and 
+        coordinating lower-level helper functions.
+        Does some optimization to run census api queries in parallel. Consider tweaking 
+        N_PROCESSES parameter to affect performance.
+        args:
+            county_names (List[str]): List of county names and their state
+            selected_cats (List[str]): List of variable categories of interest
+            descriptions (boolean): Boolean controlling whether to include variable
+                descriptions in df output (not implemented)
+            src (str): Census api source parameter       
+        """
+
+        # generate list of selected census api variable ids
+
+        all_vars = list(vardf[vardf.category.isin(selected_cats)].vars)
+
+        tabletypes = [
+            ("B", r"detail"),
+            ("S", r"subject"),
+            ("DP", r"profile"),
+            ("CP", r"cprofile"),
+        ]
+
+        # Within one census api query, all vars must be from same table type &
+        # from same year. So we make one call to 
+        # censusdata.download for each year x tabletype.  
+
+        # So:
+        # 1. get state & county fips - county formatted as list for potential update to allow users to select multiple counties
+
+        state_fip = list(state_county_fips[state_county_fips['State']==state].State_FIPS.unique())[0]
+        if len(state_fip)==1:
+            state_fip= '0' + state_fip
+
+        badcounty_fips = list(state_county_fips[state_county_fips['County'].isin(county_names)].County_FIPS)
+        county_fips = []
+        for county in badcounty_fips:
+            if len(county)==1:
+                county_fips.append('00' + county)
+            elif len(county)==2:
+                county_fips.append('0' + county)
+            else:
+                county_fips.append(county)
+        # 2. build list of tabletypes (& corresponding vars)
+
+        tabletype_jobs = []
+        for table_prefix, tabletype in tabletypes:
+
+            tabletype_vars = [var for var in all_vars if var.startswith(table_prefix)]
+
+            if tabletype_vars:
+                tabletype_jobs.append([tabletype_vars, tabletype])
+
+        # 3. get two years, five years apart, of most recently available census data
+        
+        year = int(date.today().strftime("%Y"))
+        start = year-6
+        end = year-2
+        years_list = list((start,end))
+
+        # 3. cross product: years x tabletypes
+
+        census_jobs = []
+
+        for years, (tabletype_vars, tabletype), county in product(
+            years_list, tabletype_jobs, county_fips
+        ):
+            census_jobs.append(
+                [state_fip, county, tabletype_vars, src, years, tabletype, self.api_key]
+            )
+
+        # 4. run all of the downloads (in parallel)
+
+        pool = Pool(N_PROCESSES)
+
+        raw_dfs = pool.starmap(self._call_fortracts, census_jobs)
+
+        # 5. filter counties and merge all 
+        # Note! right now it is setup assuming only one county is selected. If users want to select multiple in the future,
+        # this section will need to be reconfigured. 
+        geos = []
+        for geo in raw_dfs[0][0].index.tolist():
+            if geo.name in county_names:
+                geos.append(geo)
+        
+        resultspast = []
+        resultspresent = []
+        for result in raw_dfs:
+            if result[1] == start:
+                resultspast.append(result[0])
+            elif result[1] == end:
+                resultspresent.append(result[0])
+
+        if len(resultspast)==1:
+            past = resultspast[0]
+            present = resultspresent[0]
+        else:
+            past = reduce(lambda df1,df2: pd.merge(df1,df2,left_index=True, right_index=True, how="outer"), resultspast)
+            present = reduce(lambda df1,df2: pd.merge(df1,df2,left_index=True, right_index=True, how="outer"), resultspresent)
+        
+        past['Year'] = start
+        present['Year'] = end
+        # grab the tract numbers into a new column
+        tractnums = [place.geo[2][1] for place in past.index]
+        past['TRACTCE'] = tractnums
+        tractnums = [place.geo[2][1] for place in present.index]
+        present['TRACTCE'] = tractnums
+
+        return (present, past, state_fip, county_fips)
--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import pandas as pd
+import geojson
 from census import CensusViewer
 from layout_styles import SIDEBAR_STYLE, TEXT_STYLE
 # Connect to main app.py file
@@ -10,15 +11,16 @@ from app import app
 from app import server
 
 # Connect to app pages
-from apps import home, mapviz, fulldatatable, vardetails, contactinfo
+from apps import data_insights, map, fulldatatable, vardetails, contactinfo
 # read in state and county FIPS codes
-state_county_choices = pd.read_csv("state_county_fips.csv")
+state_county_choices = pd.read_csv("state_county_data.csv")
 # read in variable choices
 vardf = pd.read_csv('census_vars_V5.csv')
 # setup census viewer
-censusViewer = CensusViewer(api_key='')
+censusViewer = CensusViewer(api_key='e12de88e1f23dcdd9a802fbbb92b362a1e67c3c4')
 # List of variable category choices
 varcategories = censusViewer.available_categories()
+
 
 # Define controls for user filters
 controls = dbc.FormGroup(
@@ -30,7 +32,7 @@ controls = dbc.FormGroup(
             id='states-dropdown', value=None, clearable=False,
             options=[{'label': x, 'value': x} for x in state_county_choices["State"].unique()],
             multi=False,
-            persistence=True
+            persistence=True, persistence_type='memory'
         ),
         html.Br(),
         html.P('Pick County', style={
@@ -41,16 +43,16 @@ controls = dbc.FormGroup(
             id='county-dropdown', value=None, clearable=False,
             options=[],
             multi=False,
-            persistence=True
+            persistence=True, persistence_type='memory'
         ),
         html.Br(),
         html.P('Pick Variable Categories', style={
             'textAlign': 'center'
         }),
         dcc.Dropdown(
-            id='vars-dropdown', value=['Race and Ethnicity'],
+            id='vars-dropdown',
             persistence=True, persistence_type='memory',
-            options=[{'label': x, 'value': x} for x in varcategories],
+            options=[{'label': x, 'value': x} for x in varcategories if x not in ['Educational Attainment', 'Population Living With Disabilities', 'Unemployment, Households with Children under 6', 'Race and Ethnicity']],
             multi=True,
         ),
         html.Br(),
@@ -60,15 +62,25 @@ controls = dbc.FormGroup(
             children='Submit',
             color='primary',
             block=True
-        ),
+        )
     ]
 )
+# Citation
+citation = html.P(['Data source:', html.Br(), '1. U.S. Census Bureau. (2016). 2011-2015 American Community Survey 5-year estimates.',
+                   html.Br(),'2. U.S. Census Bureau. (2017). 2012-2016 American Community Survey 5-year estimates.',
+                   html.Br(), '3. U.S. Census Bureau. (2018). 2013-2017 American Community Survey 5-year estimates.',
+                   html.Br(), '4. U.S. Census Bureau. (2019). 2014-2018 American Community Survey 5-year estimates',
+                   html.Br(), '5. U.S. Census Bureau. (2020). 2015-2019 American Community Survey 5-year estimates.',
+                   html.Br(), "Retrieved using Census API ('https://api.census.gov/data/')"], style={
+            'textAlign': 'left', 'fontSize': 11
+        })
 # Compile sidebar
 sidebar = html.Div(
     [
         html.H3('Filter a Data Selection', style=TEXT_STYLE),
         html.Hr(),
-        controls
+        controls,
+        citation
     ],
     style=SIDEBAR_STYLE,
 )
@@ -76,11 +88,11 @@ sidebar = html.Div(
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div([
-        dcc.Link('Data Insights |', href='/apps/home'),
-        dcc.Link(' Map |', href='/apps/mapviz'),
-        dcc.Link(' Raw Data |', href='/apps/fulldatatable'),
-        dcc.Link(' Variable Details |', href='/apps/vardetails'),
-        dcc.Link(' Contact ', href='/apps/contactinfo'),
+        dcc.Link('Data Insights |', href='/apps/data_insights'),
+        dcc.Link(' Map |', href='/apps/map'),
+        dcc.Link(' Raw Data |', href='/apps/raw_data'),
+        dcc.Link(' Variable Details |', href='/apps/var_details'),
+        #dcc.Link(' Contact ', href='/apps/contactinfo'),
     ], style={'font-size': 'xx-large'}),
     html.Div([html.Img(id="logo", src=app.get_asset_url("NHSAlogo.png"))], style={'position': 'fixed', 'top':0, 'right':0}),
     html.Div([sidebar]),
@@ -105,21 +117,22 @@ def set_county_options(chosen_state):
                Input('states-dropdown', 'value')])
 def display_page(pathname, svars, scounties, sstates):
     # format user selections for census api calls
+    # scounties and sstates are put inside list brackets since currently users can only choose one county; census py supports multiple counties however
     fulldf = censusViewer.build_dataframe(county_names=[scounties], states=[sstates], selected_cats=svars)
     cols = [{"name": i, "id": i} for i in fulldf.columns]
     dat = fulldf.to_dict('records')
-    if pathname == '/apps/home':
-        return home.return_insights(fulldf)
-    if pathname == '/apps/mapviz':
-        return mapviz.layout
-    if pathname == '/apps/fulldatatable':
+    if pathname == '/apps/data_insights':
+        return data_insights.return_insights(fulldf)
+    if pathname == '/apps/map':
+        return map.return_maps(svars, scounties, sstates, state_county_choices, vardf)
+    if pathname == '/apps/raw_data':
         return fulldatatable.return_content(cols, dat)
-    if pathname == '/apps/vardetails':
+    if pathname == '/apps/var_details':
         return vardetails.return_contentvars(highlightcats=svars)
     if pathname == '/apps/contactinfo':
         return contactinfo.layout
     else:
-        return home.return_insights(fulldf)
+        return data_insights.return_insights(fulldf)
 
 
 if __name__ == '__main__':
